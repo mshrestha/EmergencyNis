19add23643861f762fa2b04fa6210f1cb1d6b023
Ruju Bajracharya <bajra.ruju@gmail.com>
2019-07-10 18:14:03 +0545

    update fixes

--- resources/views/layouts/partials/alert.blade.php
+++ resources/views/layouts/partials/alert.blade.php
@@ -1,5 +1,5 @@
 @if(Session::has('notify_message'))
-    <div class="alert alert-{{ Session::get('notify_type') }}">
+    <div class="alert alert-custom alert-{{ Session::get('notify_type') }}">
         <button type="button" class="close" data-dismiss="alert"><span>&times;</span><span class="sr-only">Close</span></button>
         <span class="text-semibold">{{ Session::get('notify_message') }}</span>
     </div>
