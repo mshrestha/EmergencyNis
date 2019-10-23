bee69fc78a06fd4075d534dd38eb8501d22d8f00
Rezaul Islam <rihasan.engr@gmail.com>
2019-10-23 12:53:29 +0600

    Based on User Role selection Facility/Multiple Facility select in User Registration

--- resources/views/user/partials/fields.blade.php
+++ resources/views/user/partials/fields.blade.php
@@ -20,7 +20,8 @@
 <div class="form-group">
     <label class="col-sm-3 control-label">Role</label>
     <div class="col-sm-9">
-        <select name="role" class="form-control" required id="role">
+        <select name="role" class="form-control" id="role" required>
+            <option value="" >Select Role</option>
             <option value="admin" {{ (isset($user) && $user->role == 'admin') ? ' selected' : '' }}>Admin</option>
             <option value="manager" {{ (isset($user) && $user->role == 'manager') ? ' selected' : '' }}>Manager</option>
             <option value="user" {{ (isset($user) && $user->role == 'user') ? ' selected' : '' }}>User</option>
@@ -42,21 +43,21 @@
     </div>
 </div>
 
-    <div class="form-group">
-        <label class="col-sm-3 control-label">Facility</label>
-        <div class="col-sm-9">
-            {{--<select name="facility_id" class="form-control" required>--}}
-            <select name="facility_id" class="form-control">
-                <option value="">Select Facility</option>
-                @foreach($facilities as $facility)
-                    <option value="{{ $facility->id }}" {{ (isset($user) && $user->facility_id == $facility->id) ? ' selected' : '' }}>{{ $facility->facility_id }}</option>
-                @endforeach
-            </select>
-        </div>
+<div class="form-group" id='user' style='display:none;'>
+    <label class="col-sm-3 control-label">Facility</label>
+    <div class="col-sm-9">
+        {{--<select name="facility_id" class="form-control" required>--}}
+        <select name="facility_id" class="form-control">
+            <option value="">Select Facility</option>
+            @foreach($facilities as $facility)
+                <option value="{{ $facility->id }}" {{ (isset($user) && $user->facility_id == $facility->id) ? ' selected' : '' }}>{{ $facility->facility_id }}</option>
+            @endforeach
+        </select>
     </div>
+</div>
 
 
-<div class="form-group">
+<div class="form-group" id='manager' style='display:none;'>
     <label class="col-sm-3 control-label">Facility (Multiple for Manager only)</label>
     <div class="col-sm-9">
         <select name="mfacility_id[]" class="form-control show-tick selectpicker"
@@ -76,3 +77,26 @@
         <input type="password" name="password" class="form-control" placeholder="Password">
     </div>
 </div>
+
+@push('scripts')
+<script>
+    $(document).ready(function () {
+        $('#role').on('change', function () {
+            if (this.value == 'manager') {
+                $("#manager").show();
+            }
+            else {
+                $("#manager").hide();
+            }
+        });
+        $('#role').on('change', function () {
+            if (this.value == 'user') {
+                $("#user").show();
+            }
+            else {
+                $("#user").hide();
+            }
+        });
+    });
+</script>
+@endpush
