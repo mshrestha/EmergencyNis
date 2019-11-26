840c600a36c8d92b7850ca981a8dacf32aa3a753
Ruju Bajracharya <bajra.ruju@gmail.com>
2019-11-26 15:22:09 +0545

    sync server

--- app/Http/Controllers/SyncDataServerController.php
+++ app/Http/Controllers/SyncDataServerController.php
@@ -8,39 +8,38 @@ use Illuminate\Http\Request;
 class SyncDataServerController extends Controller
 {
 	public function syncChildrenServer(Request $request) {
-		DB::beginTransaction();
-		try {
-			$datas = unserialize($request->children_sync);
-			$children_synced_ids = [];
-
-			foreach($datas as $data) {
-				$data['sync_status'] = 'synced';
-				unset($data['id']);
-
-				DB::table('children')->updateOrInsert(['sync_id' => $data['sync_id']], $data);
-				array_push($children_synced_ids, $data['sync_id']);
-			}
-
-			DB::commit();
-		} catch (Exception $e) {
-			DB::rollback();
-		}
-
-		return $children_synced_ids;
+		return $this->updateDatabase('children', $request);
 	}
 
 	public function syncFacilityFollowupServer(Request $request) {
+		return $this->updateDatabase('facility_followups', $request);
+	}
+
+	public function syncIycfFollowupServer(Request $request) {
+		return $this->updateDatabase('iycf_followups', $request);
+	}
+
+	public function syncPregnantWomenServer(Request $request) {
+		return $this->updateDatabase('pregnant_womens', $request);
+	}
+
+	public function syncPregnantWomenFollowupServer(Request $request) {
+		return $this->updateDatabase('pregnant_women_followups', $request);
+	}
+
+
+	public function updateDatabase($table, Request $request) {
 		DB::beginTransaction();
 		try {
-			$datas = unserialize($request->facility_followup_sync);
-			$facility_followup_synced_ids = [];
+			$datas = unserialize($request->sync_data);
 
+			$synced_ids = [];
 			foreach($datas as $data) {
 				$data['sync_status'] = 'synced';
 				unset($data['id']);
 
-				DB::table('facility_followups')->updateOrInsert(['sync_id' => $data['sync_id']], $data);
-				array_push($facility_followup_synced_ids, $data['sync_id']);
+				DB::table($table)->updateOrInsert(['sync_id' => $data['sync_id']], $data);
+				array_push($synced_ids, $data['sync_id']);
 			}
 
 			DB::commit();
@@ -48,6 +47,6 @@ class SyncDataServerController extends Controller
 			DB::rollback();
 		}
 
-		return $facility_followup_synced_ids;
+		return $synced_ids;
 	}
 }
