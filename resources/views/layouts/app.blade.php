e1c5ecca4fa6577f6767752a253927f0dad0c93a
Rezaul Islam <rihasan.engr@gmail.com>
2019-12-04 16:37:11 +0600

    supply module menu added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -88,6 +88,10 @@
                     <a href="{{ route('register') }}" style="color: #fff;"><i class="fa fa-id-badge"></i> <span
                                 class="nav-label">Register</span></a>
                 </li>
+                <li class="{{ request()->segment(1) == 'supply' ? 'active' : '' }}">
+                    <a href="{{ route('supply.index') }}" style="color: #fff;"><i class="fa fa-id-badge"></i> <span
+                                class="nav-label">Supply</span></a>
+                </li>
             </ul>
 
         </div>
