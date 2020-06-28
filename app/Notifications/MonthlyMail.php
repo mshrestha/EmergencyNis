80cf4ccf8f71134691cce3b27cb040af53e6a543
mrihasan <rihasan.engr@gmail.com>
2020-06-28 23:16:11 +0600

    Monthly mail May-20 2nd version updated

--- app/Notifications/MonthlyMail.php
+++ app/Notifications/MonthlyMail.php
@@ -46,27 +46,24 @@ class MonthlyMail extends Notification
         return (new MailMessage)
             ->greeting('Dear '.$this->employee_info->full_name)
             ->subject('[Auto Generated] Monthly Nutrition Sector Report, MAY 2020')
-            ->line('Under the dynamic leadership and guidance of our honorable Minister, respectable Secretary sir, 
-            HSD, Ministry of Health and Family welfare and respectable DG sir of DGHS Prof. Abul Kalam Azad, 
-            National Nutrition Services (NNS), Institute of Public Health Nutrition (IPHN) and Civil Surgeon, Cox’s Bazar, 
-            the Nutrition Sector is pleased to share Monthly Online Emergency Nutrition Report (ENR) for May 2020.')
-            ->line('This report is automatically generated from online nutrition sector portal https://emergencynutrition.org/ 
-            with data shared by all nutrition sector partners using Emergency Nutrition Information Module (ENIM) and other data systems. 
-            For detailed report and status by each implementing partner or facility, please visit the above website.')
+            ->line('Under the dynamic leadership of Civil Surgeon, Cox’s Bazar, the Nutrition Sector is pleased to share 
+            Monthly Online Emergency Nutrition Report (ENR) for May 2020. This report is automatically generated from online nutrition 
+            sector portal https://emergencynutrition.org/ with data shared by all nutrition sector partners using 
+            Emergency Nutrition Information Module (ENIM) and other data systems. For detailed report and status by each implementing 
+            partner or facility, please visit the above website.')
             ->line('<img src="http://beta.emergencynutrition.org/img/may_2020.jpg">')
-            ->line('• 18,898 for malnourished boys and girls under five, pregnant and lactating women were admitted 
-            to the nutrition treatment and preventive services during May 2020. It includes 872 SAM children; 5,693 MAM children 
-            and 149 MAM pregnant and lactating women.')
-            ->line('• 176,935 boys and girls aged 6-59 months and PLW reached with Blanket Supplementary Feeding Programme.')
-            ->line('• 21,161 new pregnant and lactating women and caregivers of children reached by IYCF counselling 
-            and participated in the IYCF group sessions.')
+            ->line('• Overall, 18,898 for malnourished boys and girls under five, pregnant and lactating women were admitted to 
+            the nutrition treatment and preventive services since May 1st 2020.')
+            ->line('• In May  872 SAM children were admitted and in April  447 SAM children were reached,  therefore the admission rate has doubled.')
+            ->line('• Also 5,693 MAM children received targeted supplementary feeding compared to 1,862 children in April-which almost 3 fold increase.')
+            ->line('• 21,161 new pregnant and lactating women and caregivers of children reached by IYCF counselling and participated in 
+            the IYCF group sessions. In May,  additional 17,830 children women were reached compared 16,958 women in April.')
             ->line('For further information, please contact with info@nutritionsector.org ')
 
             ->attach(public_path('pdf/may_2020.pdf'), [
                 'as' => 'may_2020.pdf',
                 'mime' => 'text/pdf',
             ]);
-
     }
 
     /**
