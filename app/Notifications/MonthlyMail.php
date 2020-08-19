ecb65208acee47e0a576d61176a631e367388f64
Rezaul Islam <rihasan.engr@gmail.com>
2020-08-19 08:57:53 +0600

    Monthly Mail ready for July 2020

--- app/Notifications/MonthlyMail.php
+++ app/Notifications/MonthlyMail.php
@@ -45,32 +45,34 @@ class MonthlyMail extends Notification
     {
         return (new MailMessage)
             ->greeting('Dear '.$this->employee_info->full_name)
-            ->subject('[Auto Generated] Monthly Nutrition Sector Report, June 2020')
-            ->line('Under the dynamic leadership of Civil Surgeon, Cox’s Bazar, the Nutrition Sector is pleased to share 
-            Monthly Online Emergency Nutrition Report (ENR) for June 2020. This report is automatically generated from online 
-            nutrition sector portal https://emergencynutrition.org/ with data shared by all nutrition sector partners using 
-            Emergency Nutrition Information Module (ENIM) and other data systems. For detailed report and status by each 
-            implementing partner or facility, please visit the above website.')
-            ->line('<img src="http://beta.emergencynutrition.org/img/june_2020.jpg">')
+            ->subject('[Auto Generated] Monthly Nutrition Sector Report, July 2020')
+            ->line('Under the dynamic leadership of Civil Surgeon, Cox’s Bazar, the Nutrition Sector is pleased to 
+            share Monthly Online Emergency Nutrition Report (ENR) for July 2020. This report is automatically generated 
+            from online nutrition sector portal https://emergencynutrition.org/ with data shared by all nutrition sector 
+            partners using Emergency Nutrition Information Module (ENIM) and other data systems. For detailed report and 
+            status by each implementing partner or facility, please visit the above website.')
+            ->line('<img src="http://beta.emergencynutrition.org/img/july_2020.jpg">')
 
-            ->line('• Overall, 29,886 for malnourished boys and girls under five, pregnant and lactating women were 
+            ->line('• Overall, 38,352 for malnourished boys and girls under five, pregnant and lactating women were 
             admitted to the nutrition treatment and preventive services since January 2020.')
-            ->line('• In June 1,597 SAM children were admitted and in May 872 SAM children were reached, therefore 
-            the admission rate has doubled.')
-            ->line('• Also 9,689 MAM children received targeted supplementary feeding compared to 5,693 children in 
-            May-which almost 2 fold increase.')
-            ->line('• 31,716 new pregnant and lactating women and caregivers of children reached by IYCF counselling 
-            and participated in the IYCF group sessions. In May, additional 10,555 children women were reached.')
-            ->line('• In addition Vitamin A supplementation activities reached over 92,000 children under five. 
-            Within the Vitamin A community intervention, close to 4,000 new SAM and MAM cases were identified and enrolled 
-            in the respective nutrition treatment programme.')
-            ->line('• In last 3 months only 5 children admitted in Stabilization Centre, whereas there are 33 children 
-            admitted on March 2020')
+            ->line('• In July 1,019 SAM children were admitted and in June 1,597 SAM children were reached, therefore 
+            the admission rate falls to one third. As of July, 1,5667 number of SAM children have been admitted, which is 41%  of the overall target')
+            ->line('• Also, 7,236 MAM children received targeted supplementary feeding compared to 9,347 children in 
+            June-which almost 29% decrease.')
+            ->line('• 3,737  new pregnant and lactating women and caregivers of children reached by IYCF counseling 
+            and participated in the IYCF group sessions. In June, 3,111 PLW reached by IYCF counselling, besides 31,716 
+            children women were reached by IYCF messaging through VAS campaign.')
+            ->line('• The Vitamin A supplementation event, held between 21 June 2020  to 20 July 2020 reached over 
+            81,462 boys and 78,564 girls under five-so the overall coverage was  97 %. During the event, fom a totally of 
+            155,217 screened for malnutrition children under five, 745 were new SAM and 6,520 new MAM children. Besides that,  
+            All children were referred to the respective nutrition (OTP, TSFP) and disability caring services.')
+            ->line('• In the last 4 months, only 15 children admitted in Stabilization Centre, whereas 33 children 
+            alone were admitted in March 2020')
 
             ->line('For further information, please contact with info@nutritionsector.org ')
 
-            ->attach(public_path('pdf/ENIMDashboardJune2020-EmergencyNutritionSectorCoxsBazar.pdf'), [
-                'as' => 'ENIMDashboardJune2020-EmergencyNutritionSectorCoxsBazar.pdf',
+            ->attach(public_path('pdf/ENIMDashboardJuly2020-EmergencyNutritionSectorCoxsBazar.pdf'), [
+                'as' => 'ENIMDashboardJuly2020-EmergencyNutritionSectorCoxsBazar.pdf',
                 'mime' => 'text/pdf',
             ]);
     }
