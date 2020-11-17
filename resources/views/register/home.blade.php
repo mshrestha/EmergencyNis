95c45de9a3e067a54fd4a8be17a66e6b33f38028
mrihasan <rihasan.engr@gmail.com>
2020-11-17 13:20:06 +0600

    menu tab fixed in registered children

--- resources/views/register/home.blade.php
+++ resources/views/register/home.blade.php
@@ -26,67 +26,7 @@
             <div class="row">
                 <div class="col-sm-12 tab-content">
                     <div class="ibox tab-pane active" id="tab-1">
-                        <div class="ibox-title">
-                            {{--<div class="col-md-12 ">--}}
-                                <h2>Registered Children</h2>
-                            <div class="pull-right " style="display: inline; top: 0; right: 0; position:absolute; padding-top: 15px; padding-right: 30px">
-
-                                <a href="{{ route('register') }}" >
-                                    <button type="button" class="btn btn-primary"><i
-                                                class="fa fa-user"></i>
-                                        All
-                                    </button>
-                                </a>
-                                <a href="{{ route('defaulter_child') }}" >
-                                    <button type="button" class="btn btn-primary"><i
-                                                class="fa fa-user"></i>
-                                        Scheduled Absent
-                                    </button>
-                                </a>
-                                <a href="{{ route('sam_child') }}" >
-                                    <button type="button" class="btn btn-danger"><i
-                                                class="fa fa-user"></i>
-                                        OTP
-                                    </button>
-                                </a>
-                                <a href="{{ route('mam_child') }}" >
-                                    <button type="button" class="btn btn-warning"><i
-                                                class="fa fa-user"></i>
-                                        TSFP
-                                    </button>
-                                </a>
-                                <a href="{{ route('normal_child') }}" >
-                                    <button type="button" class="btn btn-info"><i
-                                                class="fa fa-user"></i>
-                                        BSFP
-                                    </button>
-                                </a>
-                                <div  class="btn-group " >
-                                    <button type="button" class="btn btn-success dropdown-toggle"
-                                            data-toggle="dropdown">
-                                        Select Facility
-                                        <span class="caret"></span>
-                                    </button>
-                                    <ul class="dropdown-menu FixedHeightContainer" role="menu">
-                                        @foreach($camp_facilities as $cf)
-                                            <li>
-                                                <a href="{{url('register_selected_facility/'.$cf->id)}}">{{$cf->facility_id}}</a>
-                                            </li>
-                                        @endforeach
-                                        <li class="divider"></li>
-                                        <li><a href="{{ url('/register') }}">All</a></li>
-                                    </ul>
-                                </div>
-
-                                <a href="{{ route('children.create') }}" >
-                                    <button type="button" class="btn btn-primary"><i
-                                                class="fa fa-plus"></i>
-                                        Add Child
-                                    </button>
-                                </a>
-                            </div>
-                                <span class="small">All child needs to be registered in order to use this system.</span>
-                        </div>
+@include('register.partials.menu_tab')
                         <div class="ibox-content">
                             <div class="clients-list">
                                 <div class="full-height-scroll">
