e1e8a01da7342962de0e709855e92e276b08563c
Ruju Bajracharya <ruju@kazistudios.com>
2021-06-29 19:44:54 +0545

    ssl fix

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -25,10 +25,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-
-
-	If (env('APP_ENV') !== 'local') {
-            $this->app['request']->server->set('HTTPS', true);
-        }
     }
 }
